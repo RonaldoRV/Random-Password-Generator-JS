a9aab0eb250e3f979959d6322181b4a14d398529
RonaldoRV <107870806+RonaldoRV@users.noreply.github.com>
2024-06-22 12:29:16 -0500

    Add files via upload

---
+++ a.js
@@ -0,0 +1,42 @@
+const passwordBox = document.getElementById("password");
+const getPasswordBtn = document.querySelector("button");
+
+const lenght = 12;
+
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz"
+const number = "0123456789";
+const symbol = "!@#$%^&*(){}][';/.,<>-+=-";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword(){
+    let password = "";
+
+    password += upperCase[Math.floor(Math.random()* upperCase.length)];
+    password += lowerCase[Math.floor(Math.random()* lowerCase.length)];
+    password += number[Math.floor(Math.random()* number.length)];
+    password += symbol[Math.floor(Math.random()* symbol.length)];
+
+    while (lenght > password.length){
+        password += allChars[Math.floor(Math.random()* allChars.length)];
+    }
+    passwordBox.value = password;
+
+}
+
+function randomPassword(){
+    let password = "";
+    password = Math.floor((Math.random()* 50));
+    passwordBox.value = password;
+}
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+}
+
+getPasswordBtn.addEventListener("click", createPassword);
+
+
